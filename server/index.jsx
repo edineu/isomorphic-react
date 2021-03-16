761e304a082ec1e9098ff9da7a0f564b3939773b
Ed Hansen <ed@neubra.dk>
2021-03-16 18:06:16 +0100

    express and webpack now is setup and ready to go

--- server/index.jsx
+++ server/index.jsx
@@ -1,10 +1,24 @@
 import express from "express";
 import yields from "express-yields";
 import fs from "fs-extra";
+import webpack from "webpack";
 
 /** here you define the post and instiate express */
 const port = process.env.PORT || 3000;
 const app = express(); /* here you instatiate the app */
+
+if (process.env.NODE_ENV === "development") {
+  const config = require("../webpack.config.dev.babel").default;
+  const compiler = webpack(config);
+
+  app.use(
+    require("webpack-dev-middleware")(compiler, {
+      noInfo: true,
+    })
+  );
+
+  app.use(require("webpack-hot-middleware")(compiler));
+}
 
 /* the * (start) is a generator function */
 app.get(["/"], function* (req, res) {
