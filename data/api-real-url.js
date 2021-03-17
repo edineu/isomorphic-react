92d55232039b11336b1f0460c2021b906d855000
Ed Hansen <ed@neubra.dk>
2021-03-17 16:43:51 +0100

    API and Data mock implementation

---
+++ data/api-real-url.js
@@ -0,0 +1,3 @@
+export const question = `https://api.stackexchange.com/2.0/questions?site=stackoverflow`;
+export const question = (id) =>
+  `https://api.stackexchange.com/2.0/questions${id}?site=stackoverflow`;
